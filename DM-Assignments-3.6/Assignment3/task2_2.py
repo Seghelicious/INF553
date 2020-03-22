27f9952041a450dd90b8e06b628aa8a9ffc92320
Deepak Jha <deepakjha@Deepaks-MBP.lan1>
2020-03-22 04:49:21 -0700

    0.9933 rmse

--- DM-Assignments-3.6/Assignment3/task2_2.py
+++ DM-Assignments-3.6/Assignment3/task2_2.py
@@ -1,110 +1,113 @@
+import math
 import os
 import sys
 import json
 import numpy as np
-import pandas as pd
 import xgboost as xgb
-from pandas import json_normalize
 from pyspark import SparkContext, SparkConf
 import time
 
 os.environ["PYSPARK_PYTHON"] = "/usr/local/bin/python3.6"
 
+
+def get_feature_list(row):
+    features = []
+    features.extend(user_json_map.get(row[0]))
+    features.extend(business_json_map.get(row[1]))
+    return features
+
+
+def write_to_file(output_file, output, test_data):
+    file = open(output_file, 'w')
+    file.write("user_id, business_id, prediction\n")
+    for i in range(0, len(output)):
+        predicted_rating = output[i]
+        predicted_rating = max(1, min(5, predicted_rating))
+        file.write(test_data[i][0] + "," + test_data[i][1] + "," + str(predicted_rating) + "\n")
+
+
 start_time = time.time()
-# time /home/local/spark/latest/bin/spark-submit task2_1.py $ASNLIB/publicdata/yelp_train.csv $ASNLIB/publicdata/yelp_val.csv task2-output.csv
-# train_folder_path = sys.argv[1]
-# input_file_test = sys.argv[2]
+# time /home/local/spark/latest/bin/spark-submit task2_2.py $ASNLIB/publicdata/ $ASNLIB/publicdata/yelp_val.csv task2_2.csv
+# train_folder = sys.argv[1]
+# test_file = sys.argv[2]
 # output_file = sys.argv[3]
 train_folder = 'dataset/'
 test_file = 'dataset/yelp_val.csv'
 output_file = 'output/task2_2.csv'
 
-
-
-
-
 conf = SparkConf().setAppName("INF553").setMaster('local[*]')
 sc = SparkContext(conf=conf)
 sc.setLogLevel("ERROR")
 
+train_rdd = sc.textFile(os.path.join(train_folder, 'yelp_train.csv'))
+train_header = train_rdd.first()
+train_data = train_rdd.filter(lambda x: x != train_header).map(lambda x: x.split(",")).map(
+    lambda x: (x[0], x[1], float(x[2])))
 
-user_file = 'dataset/user.json'
-business_file = 'dataset/business.json'
-yelp_train = 'dataset/yelp_train.csv'
+user_json_map = sc.textFile(os.path.join(train_folder, 'user.json')).map(json.loads)
+user_json_map = user_json_map.map(
+    lambda x: (x["user_id"], (x["review_count"], x["average_stars"], x["useful"], x["fans"]))).collectAsMap()
+business_json_map = sc.textFile(os.path.join(train_folder, 'business.json')).map(json.loads)
+business_json_map = business_json_map.map(
+    lambda x: (x['business_id'], (x['stars'], x['review_count']))).collectAsMap()
 
+training_data = []
+label = []
+for train_row in train_data.collect():
+    training_data.append(get_feature_list(train_row))
+    label.append(train_row[2])
 
+training_data = np.asarray(training_data)
+label = np.asarray(label)
 
+train_data = xgb.DMatrix(training_data, label=label)
 
+model = xgb.train({'eta': 0.3, 'booster': 'gbtree', 'max-depth': 15, 'objective': 'reg:linear', 'silent': 1}, train_data, 50)
 
+test_rdd = sc.textFile(test_file)
+test_rdd_header = test_rdd.first()
+test_data = test_rdd.filter(lambda x: x != test_rdd_header)
+test_data_val = test_data.map(lambda x: x.split(',')).map(lambda x: (x[0], x[1])).collect()
 
+test_data_features = []
+for test_row in test_data_val:
+    test_data_features.append(get_feature_list(test_row))
+test_data_features = np.asarray(test_data_features)
 
+output = model.predict(xgb.DMatrix(test_data_features))
+write_to_file(output_file, output, test_data_val)
 
-train_data = pd.read_csv(os.path.join(train_folder, 'yelp_train.csv'))
-test_data = pd.read_csv(test_file)
+output_rdd = sc.textFile(output_file)
+output_header = output_rdd.first()
+output_data = output_rdd.filter(lambda x: x != output_header).map(lambda x: x.split(','))
+output_data_dict = output_data.map(lambda x: (((x[0]), (x[1])), float(x[2])))
+test_data_dict = test_data.map(lambda x: x.split(",")).map(lambda x: (((x[0]), (x[1])), float(x[2])))
+joined_data = test_data_dict.join(output_data_dict).map(lambda x: (abs(x[1][0] - x[1][1])))
 
-train_user_ratings = pd.DataFrame(train_data.groupby(['user_id'])['stars'].mean())
-train_user_ratings['user_rating_count'] = train_data.groupby('user_id')['stars'].count()
-train_user_ratings = train_user_ratings.reset_index()
-train_user_ratings = train_user_ratings.rename(columns={"stars": "user_average_rating"})
+diff_0_to_1 = joined_data.filter(lambda x: x >= 0 and x < 1).count()
+diff_1_to_2 = joined_data.filter(lambda x: x >= 1 and x < 2).count()
+diff_2_to_3 = joined_data.filter(lambda x: x >= 2 and x < 3).count()
+diff_3_to_4 = joined_data.filter(lambda x: x >= 3 and x < 4).count()
+diff_more_than_4 = joined_data.filter(lambda x: x >= 4).count()
+print(">=0 and <1: ", diff_0_to_1)
+print(">=1 and <2: ", diff_1_to_2)
+print(">=2 and <3: ", diff_2_to_3)
+print(">=3 and <4: ", diff_3_to_4)
+print(">=4: ", diff_more_than_4)
+rmse_rdd = joined_data.map(lambda x: x ** 2).reduce(lambda x, y: x + y)
+rmse = math.sqrt(rmse_rdd / output_data_dict.count())
+print("RMSE", rmse)
 
-# Loading user.json to dataframe (Features : review_count, average_stars, useful)
-# user_json_map = sc.textFile(os.path.join(train_folder, 'user.json')).map(json.loads)
-# user_json_map = user_json_map.map(
-#     lambda x: (x["user_id"], x["review_count"], x["average_stars"], x["useful"], x["fans"])).collectAsMap()
-# business_json_map = sc.textFile(os.path.join(train_folder, 'user.json')).map(json.loads)
-# business_json_map = business_json_map.map(
-#     lambda x: (x['business_id'], [x['stars'], x['review_count']])).collectAsMap()
-
-
-
-
-df_user_json = user_json_map.collect()
-df_user_json = pd.DataFrame(df_user_json)
-df_user_json.to_csv('user_features.csv', index=False)  # writing to csv for dataframe load
-df_user_json = pd.read_csv('user_features.csv')
-df_user_json = df_user_json.rename(
-    columns={'0': "user_json_userid", '1': "user_json_reviewcount", '2': "user_json_averagestars",
-             '3': "user_json_useful", '4': "user_json_fans"})
-
-# Loading business.json to dataframe (Features : review_count, stars, is_open)
-
-business_data = pd.read_json(os.path.join(train_folder, 'business.json'), lines=True, orient='records')
-df_business_json = pd.DataFrame(business_data)
-# df_business_json = json_normalize(df_business_json.to_dict())
-
-
-# Joining yelp_train, with yelp_train(with average stars and review count), user.json and business.json
-df_train = train_data.join(train_user_ratings, lsuffix='_train', rsuffix='_train_usersummary', how='inner')
-df_train = df_train.join(df_business_json, lsuffix='_train', rsuffix='_businessjson', how='inner')
-df_train = df_train.join(df_user_json, lsuffix='_train', rsuffix='_userjson', how='inner')
-
-df_test = test_data.join(train_user_ratings, lsuffix='_test', rsuffix='_train_usersummary', how='left')
-df_test = df_test.join(df_business_json, lsuffix='_test', rsuffix='_businessjson', how='left', )
-df_test = df_test.join(df_user_json, lsuffix='_test', rsuffix='_userjson', how='left')
-
-df_test['user_average_rating'].fillna(np.nanmean(df_test['user_average_rating']))
-df_test['user_rating_count'].fillna(np.nanmean(df_test['user_rating_count']))
-
-X_train = df_train[
-    ['user_average_rating', 'user_rating_count', 'review_count', 'is_open',
-     'user_json_reviewcount', 'user_json_averagestars', 'user_json_useful', 'user_json_fans']]
-Y_train = df_train['stars']
-
-model = xgb.XGBRegressor(label=0.5, eval_metric="auc", booster="gbtree", objective="binary:logistic", eta=0.0202048, max_depth=5, subsample = 0.6815, colsample_bytree = 0.701, silent = 1)
-model.fit(X_train, Y_train)
-
-X_test = df_test[
-    ['user_average_rating', 'user_rating_count', 'review_count', 'is_open',
-     'user_json_reviewcount', 'user_json_averagestars', 'user_json_useful', 'user_json_fans']]
-output = model.predict(data=X_test)
-
-result_df = pd.DataFrame()
-result_df['user_id'] = df_test['user_id_test']
-result_df['business_id'] = df_test['business_id_test']
-result_df['prediction'] = pd.Series(output)
-result_df['rating'] = df_test['stars']
-
-result_df.to_csv(output_file, encoding='utf-8', index=False)
-
-print(((result_df.prediction - result_df.rating) ** 2).mean() ** .5)
 print("Duration : ", time.time() - start_time)
+
+# >=0 and <1 : 101714
+# >=1 and <2 : 33288
+# >=2 and <3 : 6222
+# >=3 and <4 : 820
+# >=4 :0
+# RMSE 0.9833564690697524
+# Duration: 56.854628801345825
+#
+# real 1m3.547s
+# user 4m1.344s
+# sys 0m4.896s
