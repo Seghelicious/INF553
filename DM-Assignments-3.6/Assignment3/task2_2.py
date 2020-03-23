89fcbd38def9b3accd0945e54816a241688d7f99
Deepak Jha <deepakjha@Deepaks-MBP.lan1>
2020-03-23 13:31:03 -0700

    all tasks submitted

--- DM-Assignments-3.6/Assignment3/task2_2.py
+++ DM-Assignments-3.6/Assignment3/task2_2.py
@@ -7,7 +7,7 @@ import xgboost as xgb
 from pyspark import SparkContext, SparkConf
 import time
 
-os.environ["PYSPARK_PYTHON"] = "/usr/local/bin/python3.6"
+# os.environ["PYSPARK_PYTHON"] = "/usr/local/bin/python3.6"
 
 
 def get_feature_list(row):
@@ -15,6 +15,21 @@ def get_feature_list(row):
     features.extend(user_json_map.get(row[0]))
     features.extend(business_json_map.get(row[1]))
     return features
+
+
+def get_model_params():
+    params = {}
+    eta = 0.3
+    booster = 'gbtree'
+    max_depth = 15
+    objective = 'reg:linear'
+    silent = 1
+    params['eta'] = eta
+    params['booster'] = booster
+    params['max-depth'] = max_depth
+    params['objective'] = objective
+    params['silent'] = silent
+    return params
 
 
 def write_to_file(output_file, output, test_data):
@@ -28,12 +43,12 @@ def write_to_file(output_file, output, test_data):
 
 start_time = time.time()
 # time /home/local/spark/latest/bin/spark-submit task2_2.py $ASNLIB/publicdata/ $ASNLIB/publicdata/yelp_val.csv task2_2.csv
-# train_folder = sys.argv[1]
-# test_file = sys.argv[2]
-# output_file = sys.argv[3]
-train_folder = 'dataset/'
-test_file = 'dataset/yelp_val.csv'
-output_file = 'output/task2_2.csv'
+train_folder = sys.argv[1]
+test_file = sys.argv[2]
+output_file = sys.argv[3]
+# train_folder = 'dataset/'
+# test_file = 'dataset/yelp_val.csv'
+# output_file = 'output/task2_2.csv'
 
 conf = SparkConf().setAppName("INF553").setMaster('local[*]')
 sc = SparkContext(conf=conf)
@@ -44,59 +59,57 @@ train_header = train_rdd.first()
 train_data = train_rdd.filter(lambda x: x != train_header).map(lambda x: x.split(",")).map(
     lambda x: (x[0], x[1], float(x[2])))
 
-user_json_map = sc.textFile(os.path.join(train_folder, 'user.json')).map(json.loads)
-user_json_map = user_json_map.map(
+user_json_rdd = sc.textFile(os.path.join(train_folder, 'user.json')).map(json.loads)
+user_json_map = user_json_rdd.map(
     lambda x: (x["user_id"], (x["review_count"], x["average_stars"], x["useful"], x["fans"]))).collectAsMap()
-business_json_map = sc.textFile(os.path.join(train_folder, 'business.json')).map(json.loads)
-business_json_map = business_json_map.map(
+business_json_rdd = sc.textFile(os.path.join(train_folder, 'business.json')).map(json.loads)
+business_json_map = business_json_rdd.map(
     lambda x: (x['business_id'], (x['stars'], x['review_count']))).collectAsMap()
 
+
+# training phase - features : review_count, average_stars, useful, fans, stars, review_count. label : rating
 training_data = []
 label = []
 for train_row in train_data.collect():
     training_data.append(get_feature_list(train_row))
     label.append(train_row[2])
-
 training_data = np.asarray(training_data)
 label = np.asarray(label)
-
 train_data = xgb.DMatrix(training_data, label=label)
-
-model = xgb.train({'eta': 0.3, 'booster': 'gbtree', 'max-depth': 15, 'objective': 'reg:linear', 'silent': 1}, train_data, 50)
+model = xgb.train(get_model_params(), train_data, 50)
 
 test_rdd = sc.textFile(test_file)
 test_rdd_header = test_rdd.first()
-test_data = test_rdd.filter(lambda x: x != test_rdd_header)
-test_data_val = test_data.map(lambda x: x.split(',')).map(lambda x: (x[0], x[1])).collect()
+test_rdd = test_rdd.filter(lambda x: x != test_rdd_header)
+test_data_val = test_rdd.map(lambda x: x.split(',')).map(lambda x: (x[0], x[1])).collect()
 
-test_data_features = []
+test_data = []
 for test_row in test_data_val:
-    test_data_features.append(get_feature_list(test_row))
-test_data_features = np.asarray(test_data_features)
-
-output = model.predict(xgb.DMatrix(test_data_features))
+    test_data.append(get_feature_list(test_row))
+test_data = np.asarray(test_data)
+output = model.predict(xgb.DMatrix(test_data))
 write_to_file(output_file, output, test_data_val)
 
-output_rdd = sc.textFile(output_file)
-output_header = output_rdd.first()
-output_data = output_rdd.filter(lambda x: x != output_header).map(lambda x: x.split(','))
-output_data_dict = output_data.map(lambda x: (((x[0]), (x[1])), float(x[2])))
-test_data_dict = test_data.map(lambda x: x.split(",")).map(lambda x: (((x[0]), (x[1])), float(x[2])))
-joined_data = test_data_dict.join(output_data_dict).map(lambda x: (abs(x[1][0] - x[1][1])))
-
-diff_0_to_1 = joined_data.filter(lambda x: x >= 0 and x < 1).count()
-diff_1_to_2 = joined_data.filter(lambda x: x >= 1 and x < 2).count()
-diff_2_to_3 = joined_data.filter(lambda x: x >= 2 and x < 3).count()
-diff_3_to_4 = joined_data.filter(lambda x: x >= 3 and x < 4).count()
-diff_more_than_4 = joined_data.filter(lambda x: x >= 4).count()
-print(">=0 and <1: ", diff_0_to_1)
-print(">=1 and <2: ", diff_1_to_2)
-print(">=2 and <3: ", diff_2_to_3)
-print(">=3 and <4: ", diff_3_to_4)
-print(">=4: ", diff_more_than_4)
-rmse_rdd = joined_data.map(lambda x: x ** 2).reduce(lambda x, y: x + y)
-rmse = math.sqrt(rmse_rdd / output_data_dict.count())
-print("RMSE", rmse)
+# output_rdd = sc.textFile(output_file)
+# output_header = output_rdd.first()
+# output_data = output_rdd.filter(lambda x: x != output_header).map(lambda x: x.split(','))
+# output_data_dict = output_data.map(lambda x: (((x[0]), (x[1])), float(x[2])))
+# test_data_dict = test_rdd.map(lambda x: x.split(",")).map(lambda x: (((x[0]), (x[1])), float(x[2])))
+# joined_data = test_data_dict.join(output_data_dict).map(lambda x: (abs(x[1][0] - x[1][1])))
+#
+# diff_0_to_1 = joined_data.filter(lambda x: x >= 0 and x < 1).count()
+# diff_1_to_2 = joined_data.filter(lambda x: x >= 1 and x < 2).count()
+# diff_2_to_3 = joined_data.filter(lambda x: x >= 2 and x < 3).count()
+# diff_3_to_4 = joined_data.filter(lambda x: x >= 3 and x < 4).count()
+# diff_more_than_4 = joined_data.filter(lambda x: x >= 4).count()
+# print(">=0 and <1: ", diff_0_to_1)
+# print(">=1 and <2: ", diff_1_to_2)
+# print(">=2 and <3: ", diff_2_to_3)
+# print(">=3 and <4: ", diff_3_to_4)
+# print(">=4: ", diff_more_than_4)
+# rmse_rdd = joined_data.map(lambda x: x ** 2).reduce(lambda x, y: x + y)
+# rmse = math.sqrt(rmse_rdd / output_data_dict.count())
+# print("RMSE", rmse)
 
 print("Duration : ", time.time() - start_time)
 
--- DM-Assignments-3.6/Assignment3/task2_3.py
+++ DM-Assignments-3.6/Assignment3/task2_3.py
@@ -1,11 +1,52 @@
+import math
 import os
 import sys
-from pyspark import SparkConf, SparkContext
+import json
+import numpy as np
+import xgboost as xgb
+from pyspark import SparkContext, SparkConf
 import time
-import math
 
-os.environ["PYSPARK_PYTHON"] = "/usr/local/bin/python3.6"
-#export PYSPARK_PYTHON=python3.6
+# os.environ["PYSPARK_PYTHON"] = "/usr/local/bin/python3.6"
+
+
+def get_feature_list(row):
+    features = []
+    features.extend(user_json_map.get(row[0]))
+    features.extend(business_json_map.get(row[1]))
+    return features
+
+
+def get_model_params():
+    params = {}
+    eta = 0.3
+    booster = 'gbtree'
+    max_depth = 15
+    objective = 'reg:linear'
+    silent = 1
+    params['eta'] = eta
+    params['booster'] = booster
+    params['max-depth'] = max_depth
+    params['objective'] = objective
+    params['silent'] = silent
+    return params
+
+
+def write_to_file_model_based(output_file, output, test_data):
+    file = open(output_file, 'w')
+    file.write("user_id, business_id, prediction\n")
+    for i in range(0, len(output)):
+        predicted_rating = output[i]
+        predicted_rating = max(1, min(5, predicted_rating))
+        file.write(test_data[i][0] + "," + test_data[i][1] + "," + str(predicted_rating) + "\n")
+
+
+def write_to_file(output_file, prediction_list):
+    f = open(output_file, 'w')
+    f.write("user_id, business_id, prediction\n")
+    for i in range(len(prediction_list)):
+        f.write(str(prediction_list[i][0][0]) + "," + str(prediction_list[i][0][1]) + "," + str(prediction_list[i][1]) + "\n")
+    f.close()
 
 
 def item_based_prediction(test_data):
@@ -15,7 +56,7 @@ def item_based_prediction(test_data):
         # Cold start (new business)
         if len(list(user_rating_map.get(user))) == 0:
             # user is new too
-            return user, business, "2.5"
+            return user, business, 2.5
         return user, business, str(user_avg_rating_map.get(user))
     else:
         users_list = list(business_rating_map.get(business))
@@ -23,7 +64,7 @@ def item_based_prediction(test_data):
         average_business_rating = business_avg_rating_map.get(business)
         if user_rating_map.get(user) is None:
             # Cold start (new user)
-            return user, business, str(average_business_rating)
+            return user, business, average_business_rating
         else:
             businesses_list = list(user_rating_map.get(user))  # list() gives list of keys in dict
             if len(businesses_list) != 0:  # user has given ratings
@@ -79,58 +120,85 @@ def item_based_prediction(test_data):
                 return user, business, min(5.0, max(0.0, prediction))
             else:
                 # new user (no such user in yelp_test.csv)
-                return user, business, str(average_business_rating)
-
-
-def write_to_file(output_file, prediction_list):
-    f = open(output_file, 'w')
-    f.write("user_id, business_id, prediction\n")
-    for i in range(len(prediction_list)):
-        f.write(str(prediction_list[i][0]) + "," + str(prediction_list[i][1]) + "," + str(prediction_list[i][2]) + "\n")
-    f.close()
+                return user, business, average_business_rating
 
 
 start_time = time.time()
 
-# time /home/local/spark/latest/bin/spark-submit task2_1.py $ASNLIB/publicdata/yelp_train.csv $ASNLIB/publicdata/yelp_val.csv task2-output.csv
-input_file_train = sys.argv[1]
-input_file_test = sys.argv[2]
+# time /home/local/spark/latest/bin/spark-submit task2_3.py $ASNLIB/publicdata/ $ASNLIB/publicdata/yelp_val.csv task2_3.csv
+train_folder = sys.argv[1]
+test_file = sys.argv[2]
 output_file = sys.argv[3]
-# input_file_train = 'dataset/yelp_train.csv'
-# input_file_test = 'dataset/yelp_val.csv'
-# output_file = 'output/task2_1.csv'
+# train_folder = 'dataset/'
+# test_file = 'dataset/yelp_val.csv'
+# output_file = 'output/task2_3.csv'
 
 conf = SparkConf().setAppName("INF553").setMaster('local[*]')
 sc = SparkContext(conf=conf)
 sc.setLogLevel("ERROR")
-train_rdd = sc.textFile(input_file_train)
+train_rdd = sc.textFile(os.path.join(train_folder, 'yelp_train.csv'))
 train_header = train_rdd.first()
 train_data = train_rdd.filter(lambda x: x != train_header).map(lambda x: x.split(','))
 
-test_rdd = sc.textFile(input_file_test)
+test_rdd = sc.textFile(test_file)
 test_header = test_rdd.first()
 test_data = test_rdd.filter(lambda x: x != test_header)
-# train_data.subtract(test_data)
 
-# test_data.collect()
 
+# ITEM BASED RECOMMENDATION
 user_rating_map = train_data.map(lambda x: ((x[0]), ((x[1]), float(x[2])))).groupByKey().sortByKey(True).mapValues(dict).collectAsMap()  # user key
 business_rating_map = train_data.map(lambda x: ((x[1]), ((x[0]), float(x[2])))).groupByKey().sortByKey(True).mapValues(dict).collectAsMap()  # business key
 user_avg_rating_map = train_data.map(lambda x: (x[0], float(x[2]))).groupByKey().mapValues(lambda x: sum(x) / len(x)).collectAsMap()  # userId <-> avg rating
 business_avg_rating_map = train_data.map(lambda x: (x[1], float(x[2]))).groupByKey().mapValues(lambda x: sum(x) / len(x)).collectAsMap()  # businessId <-> avg rating
-
 test_matrix = test_data.map(lambda x: x.split(",")).sortBy(lambda x: ((x[0]), (x[1]))).persist()
-prediction_list = test_matrix.map(item_based_prediction).collect()
+prediction_item_based = test_matrix.map(item_based_prediction).map(lambda x: ((x[0], x[1]), float(x[2])))
+print("itembased done")
 
-write_to_file(output_file, prediction_list)
+# MODEL BASED RECOMMENDATION
+user_json_rdd = sc.textFile(os.path.join(train_folder, 'user.json')).map(json.loads)
+user_json_map = user_json_rdd.map(
+    lambda x: (x["user_id"], (x["review_count"], x["average_stars"], x["useful"], x["fans"]))).collectAsMap()
+business_json_rdd = sc.textFile(os.path.join(train_folder, 'business.json')).map(json.loads)
+business_json_map = business_json_rdd.map(
+    lambda x: (x['business_id'], (x['stars'], x['review_count']))).collectAsMap()
+training_data = []
+label = []
+for train_row in train_data.collect():
+    training_data.append(get_feature_list(train_row))
+    label.append(train_row[2])
+training_data = np.asarray(training_data)
+label = np.asarray(label)
+trained_data = xgb.DMatrix(training_data, label=label)
+model = xgb.train(get_model_params(), trained_data, 50)
 
-# output_rdd = sc.textFile(output_file)
-# output_header = output_rdd.first()
-# output_data = output_rdd.filter(lambda x: x != output_header).map(lambda x: x.split(','))
-# output_data_dict = output_data.map(lambda x: (((x[0]), (x[1])), float(x[2])))
-# test_data_dict = test_data.map(lambda x: x.split(",")).map(lambda x: (((x[0]), (x[1])), float(x[2])))
-# joined_data = test_data_dict.join(output_data_dict).map(lambda x: (abs(x[1][0] - x[1][1])))
-#
+test_rdd = sc.textFile(test_file)
+test_rdd_header = test_rdd.first()
+test_rdd = test_rdd.filter(lambda x: x != test_rdd_header)
+test_data_val = test_rdd.map(lambda x: x.split(',')).map(lambda x: (x[0], x[1])).collect()
+
+test_data = []
+for test_row in test_data_val:
+    test_data.append(get_feature_list(test_row))
+test_data = np.asarray(test_data)
+prediction = model.predict(xgb.DMatrix(test_data))
+temp_file = "model_based_temp_output.csv"
+write_to_file_model_based(temp_file, prediction, test_data_val)
+print("modelbased done")
+
+output_rdd = sc.textFile(temp_file)
+output_header = output_rdd.first()
+output_data = output_rdd.filter(lambda x: x != output_header).map(lambda x: x.split(','))
+prediction_model_based = output_data.map(lambda x: (((x[0]), (x[1])), float(x[2])))
+
+
+# HYBRID BASED RECOMMENDATION
+output_hybrid = prediction_item_based.join(prediction_model_based).map(lambda x: ((x[0]), float((x[1][0] * 0.1 + x[1][1] * 0.9))))
+write_to_file(output_file, output_hybrid.collect())
+print("hybridbased done")
+
+
+# test_data_dict = test_rdd.map(lambda x: x.split(",")).map(lambda x: (((x[0]), (x[1])), float(x[2])))
+# joined_data = test_data_dict.join(output_hybrid).map(lambda x: (abs(x[1][0] - x[1][1])))
 # diff_0_to_1 = joined_data.filter(lambda x: x >= 0 and x < 1).count()
 # diff_1_to_2 = joined_data.filter(lambda x: x >= 1 and x < 2).count()
 # diff_2_to_3 = joined_data.filter(lambda x: x >= 2 and x < 3).count()
@@ -142,20 +210,7 @@ write_to_file(output_file, prediction_list)
 # print(">=3 and <4: ", diff_3_to_4)
 # print(">=4: ", diff_more_than_4)
 # rmse_rdd = joined_data.map(lambda x: x ** 2).reduce(lambda x, y: x + y)
-# rmse = math.sqrt(rmse_rdd / output_data_dict.count())
+# rmse = math.sqrt(rmse_rdd / output_hybrid.count())
 # print("RMSE", rmse)
 
 print("Duration : ", time.time() - start_time)
-
-# >=0 and <1:  94269
-# >=1 and <2:  38736
-# >=2 and <3:  7755
-# >=3 and <4:  1283
-# >=4:  1
-# RMSE 1.070173169614003
-# Duration :  77.43102025985718
-# Vocareum
-# Duration :  80.88700485229492
-# real    1m23.310s
-# user    0m24.456s
-# sys     0m2.748s
